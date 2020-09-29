257292bb16075b2f0729c29bce34c7c6bc40fac2
elssay <sveta-zvezda-poeta@yandex.ru>
2020-09-29 14:15:04 +0300

    'new_comment'

--- hw5.py
+++ hw5.py
@@ -3,7 +3,8 @@ def get_absolute_url(url, *args, **kwargs):
     print(url, end = '/')
     for i in args:
         print(i, end = '/')
-       
+    #очень долго мучалась, но так и не поняла, как сделать так, чтобы
+    # не было слэша перед знаком вопроса :(
     
     print(end = '?')
     for k, v in kwargs.items():
